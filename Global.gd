a248913eb5d279516915b9a3e9ed26d3f475577b
Seemant Singh <seemantsinghsanwal@gmail.com>
2025-06-05 20:38:36 +0530

    level winner

--- Global.gd
+++ Global.gd
@@ -1,9 +1,10 @@
 extends Node
 
 var winner = "Player 1"
-var round_number = 1
+var round_number = 0
 var coin_p1 = 0
 var coin_p2 = 0
 
 var ball_by = ""
 
+var level_winner = {"1":"", "2":"", "3":""}
--- line_right.gd
+++ line_right.gd
@@ -12,6 +12,8 @@ func _on_body_entered(body:Node2D) -> void:
 	score += 1
 	if score > 5:
 		if Global.round_number < 3:
+			print("Round: " + str(Global.round_number))
+			Global.level_winner[str(Global.round_number)] = "Player 1"
 			Global.round_number += 1
 			Global.coin_p1 = 0
 			Global.coin_p2 = 0
@@ -19,8 +21,28 @@ func _on_body_entered(body:Node2D) -> void:
 			score = 0
 			get_node("../p1score").text = str(0)
 			get_node("../round_label").text = "Round: " + str(Global.round_number)
+			print(Global.level_winner)
 		else:
-			Global.winner = "Player 1"
+			Global.level_winner[str(Global.round_number)] = "Player 1"
+			Global.winner = get_match_winner(Global.level_winner)
 			get_tree().change_scene_to_file("res://end.tscn")
 	else:
 		get_node("../p1score").text = str(score)
+
+func get_match_winner(level_winner: Dictionary) -> String:
+	var count_player1 = 0
+	var count_player2 = 0
+
+	for key in level_winner:
+		var player = level_winner[key]
+		if player == "Player 1":
+			count_player1 += 1
+		elif player == "Player 2":
+			count_player2 += 1
+
+	if count_player1 > count_player2:
+		return "Player1"
+	elif count_player2 > count_player1:
+		return "Player2"
+	else:
+		return "Draw"
