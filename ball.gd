8afe235cfbd61c5ef116a479aa54a440b421e72b
Seemant Singh <seemantsinghsanwal@gmail.com>
2025-06-06 02:13:48 +0530

    reset on paddles

--- ball.gd
+++ ball.gd
@@ -8,7 +8,8 @@ var speed_dict = {
 }
 var initial_position = Vector2(640, 320)
 var should_reset = false
-
+var ball_on_paddle = true
+var _paddle_position
 
 
 func _ready() -> void:
@@ -18,14 +19,27 @@ func _ready() -> void:
 	print('ball by: ', Global.ball_by)
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-
-	state.linear_velocity = linear_velocity.normalized() * speed_dict["round_" + str(Global.round_number)]
+	if Input.is_action_just_pressed("launch_1") and _paddle_position.name == 'player_1':
+		ball_on_paddle = false
+	elif Input.is_action_just_pressed("launch_2") and _paddle_position.name == 'player_2':
+		ball_on_paddle = false
+	if ball_on_paddle:
+		var next_transform = state.get_transform()
+		if _paddle_position.name == 'player_1':
+			next_transform.origin.x = _paddle_position.global_position.x + 25
+		else:
+			next_transform.origin.x = _paddle_position.global_position.x - 25
+		next_transform.origin.y = _paddle_position.global_position.y
+		state.set_transform(next_transform)
+	else: 
+		state.linear_velocity = linear_velocity.normalized() * speed_dict["round_" + str(Global.round_number)]
 
 	if should_reset:
-		var next_transform = state.get_transform()
-		next_transform.origin.x = initial_position.x
-		next_transform.origin.y = initial_position.y
-		state.set_transform(next_transform)
+		# var next_transform = state.get_transform()
+		# next_transform.origin.x = initial_position.x
+		# next_transform.origin.y = initial_position.y
+		# state.set_transform(next_transform)
+		ball_on_paddle = true
 		should_reset = false
 	for i in get_colliding_bodies():
 		if i is CharacterBody2D:
@@ -34,4 +48,8 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 
 func _reset_ball():
 	print('reset ball')
+	ball_on_paddle = true
+	var Player1 = get_node("../player_1")
+	var Player2 = get_node("../player_2")
+	_paddle_position = Player1 if randi() % 2 == 0 else Player2
 	should_reset = true
