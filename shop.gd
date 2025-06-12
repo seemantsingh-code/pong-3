8d3d51c8f77b8848ea361353efc4432011141ad0
Seemant Singh <seemantsinghsanwal@gmail.com>
2025-06-13 02:03:38 +0530

    confused shop but sleepy

--- shop.gd
+++ shop.gd
@@ -1,10 +1,65 @@
 extends Node2D
 
+const P1_LEFT = "ui_p1_left"
+const P1_RIGHT = "ui_p1_right"
+const P1_PLAY = "ui_p1_down"
+const P2_LEFT = "ui_p2_left"
+const P2_RIGHT = "ui_p2_right"
+const P2_PLAY = "ui_p2_down"
+
+const p1_buttons = ["buy-1", "buy-3", "play-1"]
+const p2_buttons = ["buy-4", "buy-5", "play-2"]
+
+var p1_index = 0
+var p2_index = 0
+
+var label_1: Label
+var label_2: Label
+
+
+
 func _ready() -> void:
     print("Shop scene is ready")
-    var label_1 = get_node("buy-1")
+    label_1 = get_node(p1_buttons[p1_index])
     label_1.label_settings.font_color = Color(0, 1, 0)  # Green color
     print("Label 1 color set to green")
-    var label_2 = get_node("buy-3")
+    label_2 = get_node(p2_buttons[p2_index])
     label_2.label_settings.font_color = Color(1, 0, 0)  # Red color
     print("Label 2 color set to red")
+
+func _process(delta: float) -> void:
+    # Handle player input for navigation
+    if Input.is_action_just_pressed(P1_LEFT):
+        print("Player 1 moved left")
+        p1_index = max(p1_index - 1, 0)
+        for i in range(len(p1_buttons)):
+            get_node(p1_buttons[i]).label_settings.font_color = Color(1, 1, 1)  # Reset to white
+        label_1 = get_node(p1_buttons[p1_index])
+        label_1.label_settings.font_color = Color(0, 1, 0)
+    elif Input.is_action_just_pressed(P1_RIGHT):
+        print("Player 1 moved right")
+        p1_index = min(p1_index + 1, len(p1_buttons) - 1)
+        for i in range(len(p1_buttons)):
+            get_node(p1_buttons[i]).label_settings.font_color = Color(1, 1, 1)  # Reset to white
+        label_1 = get_node(p1_buttons[p1_index])
+        label_1.label_settings.font_color = Color(0, 1, 0)
+    elif Input.is_action_just_pressed(P1_PLAY):
+        print("Player 1 moved down")
+        p1_index = min(p1_index + 1, len(p1_buttons) - 1)
+        for i in range(len(p1_buttons)):
+            get_node(p1_buttons[i]).label_settings.font_color = Color(1, 1, 1)  # Reset to white
+        label_1 = get_node(p1_buttons[p1_index])
+        label_1.label_settings.font_color = Color(0, 1, 0)
+
+    if Input.is_action_just_pressed(P2_LEFT):
+        print("Player 2 moved left")
+        label_2 = get_node("buy-4")
+        label_2.label_settings.font_color = Color(1, 0, 0)
+    elif Input.is_action_just_pressed(P2_RIGHT):
+        print("Player 2 moved right")
+        label_2 = get_node("buy-5")
+        label_2.label_settings.font_color = Color(1, 0, 0)
+    elif Input.is_action_just_pressed(P2_PLAY):
+        print("Player 2 pressed play")
+        label_2 = get_node("play-2")
+        label_2.label_settings.font_color = Color(1, 0, 0)
--- shop_2.gd
+++ shop_2.gd
@@ -1,5 +1,80 @@
 extends Control
 
+# Player 1 Controls
+const P1_LEFT = "ui_p1_left"
+const P1_RIGHT = "ui_p1_right"
+const P1_PLAY = "ui_p1_down"
+
+# Player 2 Controls
+const P2_LEFT = "ui_p2_left"
+const P2_RIGHT = "ui_p2_right"
+const P2_PLAY = "ui_p2_down"
+
+# Store buttons in arrays for indexing
+var p1_buttons = []
+var p2_buttons = []
+
+var p1_index = 0
+var p2_index = 0
+
+func _ready():
+	# Populate button arrays
+	p1_buttons = [$HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/Button, $HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer2/Button]
+	p2_buttons = [$HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/Button, $HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer2/Button]
+
+	# Set P1 buttons to only accept focus manually
+	for btn in p1_buttons:
+		btn.focus_mode = Control.FOCUS_NONE
+
+	# Set P2 buttons to only accept focus manually
+	for btn in p2_buttons:
+		btn.focus_mode = Control.FOCUS_NONE
+
+	# Manually assign focus (only works with FOCUS_NONE + grab_focus)
+	p1_buttons[p1_index].focus_mode = Control.FOCUS_ALL
+	p2_buttons[p2_index].focus_mode = Control.FOCUS_ALL
+
+	# Set initial focus
+	p1_buttons[p1_index].grab_focus()
+	p2_buttons[p2_index].grab_focus()
+
+func _unhandled_input(event):
+	# --- Player 1 Controls ---
+	if Input.is_action_just_pressed(P1_LEFT):
+		p1_buttons[p1_index].focus_mode = Control.FOCUS_NONE
+		p1_index = max(p1_index - 1, 0)
+		p1_buttons[p1_index].focus_mode = Control.FOCUS_ALL
+		p1_buttons[p1_index].grab_focus()
+	elif Input.is_action_just_pressed(P1_RIGHT):
+		p1_buttons[p1_index].focus_mode = Control.FOCUS_NONE
+		p1_index = min(p1_index + 1, p1_buttons.size() - 1)
+		p1_buttons[p1_index].focus_mode = Control.FOCUS_ALL
+		p1_buttons[p1_index].grab_focus()
+	elif Input.is_action_just_pressed(P1_PLAY):
+		$HBoxContainer/VBoxContainer/Button.grab_focus()
+
+	# --- Player 2 Controls ---
+	if Input.is_action_just_pressed(P2_LEFT):
+		p2_buttons[p2_index].focus_mode = Control.FOCUS_NONE
+		p2_index = max(p2_index - 1, 0)
+		p2_buttons[p2_index].focus_mode = Control.FOCUS_ALL
+		p2_buttons[p2_index].grab_focus()
+	elif Input.is_action_just_pressed(P2_RIGHT):
+		p2_buttons[p2_index].focus_mode = Control.FOCUS_NONE
+		p2_index = min(p2_index + 1, p2_buttons.size() - 1)
+		p2_buttons[p2_index].focus_mode = Control.FOCUS_ALL
+		p2_buttons[p2_index].grab_focus()
+	elif Input.is_action_just_pressed(P2_PLAY):
+		$HBoxContainer2/VBoxContainer/Button.grab_focus()
+
+# Connect the Play buttons in the editor to this function
+func _on_PlayButtonP1_pressed():
+	print("Player 1 pressed Play")
+	# get_tree().change_scene_to_file("res://game.tscn")
+
+func _on_PlayButtonP2_pressed():
+	print("Player 2 pressed Play")
+	# get_tree().change_scene_to_file("res://game.tscn")
 
 func _on_button_pressed() -> void:
 	pass # Replace with function body.
