8a73bf6f24c210a359aa0d01bfca2f4ccd27e2bb
Seemant Singh <seemantsinghsanwal@gmail.com>
2025-06-18 01:20:43 +0530

    powerups not working at all

--- Global.gd
+++ Global.gd
@@ -18,3 +18,29 @@ const power_dict = {
 
 var p1_powerups = []
 var p2_powerups = []
+
+func _power_activate(powerup: String, player: int) -> void:
+	if player == 1:
+		if powerup in p1_powerups:
+			give_powerup(powerup, get_node("Player1"))
+			p1_powerups.erase(powerup)
+			print("Player 1 activated powerup: ", powerup)
+	else:
+		if powerup in p2_powerups:
+			give_powerup(powerup, get_node("Player2"))
+			p2_powerups.erase(powerup)
+			print("Player 2 activated powerup: ", powerup)
+var ball_on_paddle = true
+
+func give_powerup(powerup: String, player: CharacterBody2D) -> void:
+	if powerup == "enlarge":
+		player.scale *= 1.5
+	elif powerup == "fasten":
+		player.speed *= 1.5
+	elif powerup == "make-small":
+		player.scale *= 0.5
+	elif powerup == "pull-ball":
+		# Implement pull ball logic here
+		print("Pull ball powerup activated for player: ", player.name)
+	else:
+		print("Unknown powerup: ", powerup)
--- game.gd
+++ game.gd
@@ -8,6 +8,9 @@ var p1_deck_index = 0
 const p2_deck_left = 'deck_left_2'
 const p2_deck_right = 'deck_right_2'
 
+const p1_use_card = 'use_card'
+const p2_use_card = 'use_card_2'
+
 var p2_deck_index = 0
 
 var card_selector_p1: TextureRect
@@ -74,6 +77,14 @@ func _process(delta: float) -> void:
 			print("P1 Deck Index: ", p1_deck_index)
 			card_selector_p1.global_position  = $p1_powerups.get_child(p1_deck_index).get_global_position()
 			card_selector_p1.visible = true
+
+		if Input.is_action_just_pressed(p1_use_card) and not Global.ball_on_paddle:
+			var selected_powerup = $p1_powerups.get_child(p1_deck_index)
+			print("P1 using powerup: ", selected_powerup.name)
+			Global._power_activate(selected_powerup.name, 1)  # Activate the powerup for Player 1
+			selected_powerup.queue_free()  # Example: remove the powerup after use
+			card_selector_p1.visible = false
+		
 	if $p2_powerups.get_child_count() > 0:
 		if Input.is_action_just_pressed(p2_deck_left):
 			p2_deck_index = (p2_deck_index - 1) % $p2_powerups.get_child_count()
@@ -85,3 +96,9 @@ func _process(delta: float) -> void:
 			print("P2 Deck Index: ", p2_deck_index)
 			card_selector_p2.global_position  = $p2_powerups.get_child(p2_deck_index).get_global_position()
 			card_selector_p2.visible = true
+		if Input.is_action_just_pressed(p2_use_card) and not Global.ball_on_paddle:
+			var selected_powerup = $p2_powerups.get_child(p2_deck_index)
+			print("P2 using powerup: ", selected_powerup.name)
+			Global._power_activate(selected_powerup.name, 2)  # Activate the powerup for Player 2
+			selected_powerup.queue_free()  # Example: remove the powerup after use
+			card_selector_p2.visible = false
